c1008cac13c024cf7ad648e85610829ea26c0d44
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-03-10 16:11:12 +0100

    first commit

---
+++ mod.ts
@@ -0,0 +1,39 @@
+// @deno-types='https://cdn.jsdelivr.net/npm/@types/ua-parser-js@0.7.36/index.d.ts'
+import { UAParser } from 'https://cdn.jsdelivr.net/npm/ua-parser-js@1.0.34/dist/ua-parser.pack.js/+esm'
+
+export default function userAgent(header: string) {
+  const userAgent = new UAParser(header)
+
+  const browser = userAgent.getBrowser()
+  const cpu = userAgent.getCPU()
+  const device = userAgent.getDevice()
+  const engine = userAgent.getEngine()
+  const os = userAgent.getOS()
+
+  return {
+    browser: {
+      name: browser.name,
+      version: browser.version,
+    },
+
+    cpu: {
+      architecture: cpu.architecture,
+    },
+
+    device: {
+      model: device.model,
+      type: device.type,
+      vendor: device.vendor,
+    },
+
+    engine: {
+      name: engine.name,
+      version: engine.version,
+    },
+
+    os: {
+      name: os.name,
+      version: os.version,
+    },
+  }
+}
