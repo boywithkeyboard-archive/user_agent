19d84b7e4bd9bb3adf370d06b1b212691149cced
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-03 14:58:50 +0200

    test: add test suite to avoid future issues

---
+++ mod.test.ts
@@ -0,0 +1,31 @@
+import userAgent from './mod.ts'
+import { assertEquals } from 'https://deno.land/std@0.182.0/testing/asserts.ts'
+
+Deno.test('userAgent', () => {
+  const data = userAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+  )
+
+  assertEquals(data, {
+    browser: {
+      name: 'Chrome',
+      version: '111.0.0.0',
+    },
+    cpu: {
+      architecture: 'amd64',
+    },
+    device: {
+      model: undefined,
+      type: undefined,
+      vendor: undefined,
+    },
+    engine: {
+      name: 'Blink',
+      version: '111.0.0.0',
+    },
+    os: {
+      name: 'Windows',
+      version: '10',
+    },
+  })
+})
